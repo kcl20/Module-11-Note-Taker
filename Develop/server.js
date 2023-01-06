409f6de5214452bae9a80d6c6bbfeec90d54cb6e
Kenny Li <112835000+kcl20@users.noreply.github.com>
2023-01-05 21:56:10 -0500

    Updated db.json

--- Develop/server.js
+++ Develop/server.js
@@ -82,5 +82,5 @@ app.delete("/api/notes/:id", (req,res) => {
 
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+  console.log(`App listening on port ${PORT}`)
 );
